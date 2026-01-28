11468595023bef18d31f01789de588f7d70dbde4
Dmytro_Gunko <dimagunko99@gmail.com>
2026-01-28 11:02:14 +0100

    add pino lib, add errors middlewares, get route

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-const message = "Hello world";
+import express from 'express';
+import pino from 'pino-http';
 
-console.log(message);
+const PORT = 3000;
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toISOString()}`);
+  next();
+});
+
+//Обробка JSON-даних
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+//клієнт звертається до неіснуючого маршруту
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+//коли виникає непередбачувана помилка
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
