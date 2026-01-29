5c48aa3e4a2d58309ad8bd3a931ff4747354167b
Dmytro_Gunko <dimagunko99@gmail.com>
2026-01-29 17:57:23 +0100

    add env veriable, structured files and folders, add getEnvVar function

--- src/index.js
+++ src/index.js
@@ -1,46 +1,6 @@
-import express from 'express';
-import pino from 'pino-http';
+//файл, з якого буде починатися виконання нашої програми
+import { startServer } from './server.js';
 
-const PORT = 3000;
-const app = express();
+startServer();
 
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toISOString()}`);
-  next();
-});
-
-//Обробка JSON-даних
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world',
-  });
-});
-
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-//клієнт звертається до неіснуючого маршруту
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
-  });
-});
-
-//коли виникає непередбачувана помилка
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Express - це популярна JavaScript бібліотека, яка призначена для розробки вебдодатків і API. Express побудований на базі Node.js і надає простий та ефективний спосіб створення серверних застосунків.
