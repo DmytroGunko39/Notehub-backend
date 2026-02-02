d2fca114362ad3a1259de8719c9a4f29a62e1d5a
Dmytro_Gunko <dimagunko99@gmail.com>
2026-02-02 17:13:25 +0100

    add models,mongoose- Schema,

---
+++ src/db/models/note.js
@@ -0,0 +1,37 @@
+// const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+const notesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    tag: {
+      type: String,
+      default: null,
+      index: true,
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true, // createdAt / updatedAt
+    versionKey: false,
+  },
+);
+
+module.exports = mongoose.model('note', notesSchema);
